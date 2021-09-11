4f2cdf67c1f58b88c83ff9a8d1623f45871bc448
chris <cnpryer@gmail.com>
2021-09-10 23:45:22 -0400

    started troubleshooting laptop environment setup

--- scripts/wsl-bootstrap.sh
+++ scripts/wsl-bootstrap.sh
@@ -1,10 +1,20 @@
+# NOTE: this file needs work
 set -e
 
 sudo apt-get update -y
 sudo apt-get upgrade -y
 sudo apt-get dist-upgrade -y
 sudo apt-get install build-essential -y
-sudo apt-get install -y curl nodejs npm yarn
+sudo apt-get install -y curl nodejs npm
 
-curl https://sh.rustup.rs -sSf
-# sh -c "$(curl -sSfL https://release.solana.com/stable/install)"
+# if permission issues see: https://www.codegrepper.com/code-examples/shell/install+yarn+ubuntu+wsl
+npm install -g yarn
+npm i @craco/craco
+
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+sh -c "$(curl -sSfL https://release.solana.com/stable/install)"
+
+# for solana if the curl fails:
+# wget http://release.solana.com/stable/solana-release-x86_64-unknown-linux-gnu.tar.bz2
+# tar xjf solana-release-x86_64-unknown-linux-gnu.tar.bz2
+# ./solana-release/bin/solana --version
