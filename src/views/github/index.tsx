eacd635213caa8ae0426338d477da3c3e8ddccd7
chris <christophpryer@gmail.com>
2021-09-13 22:04:45 -0400

    added gatekeeper POC (#10)

--- src/views/github/index.tsx
+++ src/views/github/index.tsx
@@ -2,26 +2,19 @@ import React, { useState } from "react";
 import { Button } from "antd";
 
 
-const client_id = process.env.REACT_APP_GITHUB_OAUTH_CLIENT_ID;
-const client_secret = process.env.REACT_APP_GITHUB_OAUTH_CLIENT_SECRET;
-const redirect_uri = process.env.REACT_APP_REDIRECT_URI;
-
-
 // TODO: use state for request validation + remove logic from view
 export const GithubOAuthView = () => {
   const [message, setMessage] = useState("");
   const windowUrl = window.location.search;
   const params = new URLSearchParams(windowUrl);
-  const token_auth_url = `https://cors-anywhere.herokuapp.com/https://github.com/login/oauth/access_token?client_id=${client_id}&client_secret=${client_secret}&redirect_uri=${redirect_uri}&code=${params.get("code")}`;
+  const token_auth_url = `http://localhost:9999/authenticate/${params.get("code")}`;
 
   const callApi = async () => {
     try {
-      const response = await fetch(token_auth_url, {mode: "cors", method: "post"});
-
-      // failing to parse as json but auth token is in network response (Faith ftw)
+      const response = await fetch(token_auth_url);
       const responseData = await response.json();
 
-      setMessage(responseData.message);
+      setMessage(responseData.token);
     } catch (error: any) {
       setMessage(error.message);
     }
